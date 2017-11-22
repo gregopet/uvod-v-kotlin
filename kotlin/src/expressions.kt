fadfbeba9ab44883fc1fae039f8f9f9f3de16e8d
Gregor Petrin <gregap@gmail.com>
2017-11-22 01:36:51 +0100

    Expressions, platforms,

---
+++ kotlin/src/expressions.kt
@@ -0,0 +1,16 @@
+import java.io.File
+
+fun x(a:Any?) {
+    var b = a
+    if (b != null) {
+        b.toString()
+    }
+    val contents = try {
+        File("foo").readText()
+    } catch (e: Exception) {
+        "Error reading file!"
+    }
+}
+
+
+fun `your mother is a hamster`() { .. }
